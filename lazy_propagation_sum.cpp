e1a6e6a39a4c1e63fb2079fe78dd052b6eb7fb06
Cho Hyunsung <jhoboss@gmail.com>
2020-12-30 16:41:00 +0900

    Update lazy_propagation_sum.cpp

--- lazy_propagation_sum.cpp
+++ lazy_propagation_sum.cpp
@@ -1,89 +1,65 @@
-// https://www.acmicpc.net/problem/10999
-
-#include <iostream>
-#include <cstdio>
 #include <vector>
-
-#define ll long long
 
 using namespace std;
 
-vector<ll>tree;
-vector<ll>lazy;
-vector<ll>d;
-
-ll init(int idx, int l, int r) {
-	if (l == r) return tree[idx] = d[l];
-	else return tree[idx] = init(idx * 2, l, (l + r) / 2) + init(idx * 2 + 1, (l + r) / 2 + 1, r);
-}
-
-void update(int idx, int start, int end, int l, int r, ll val) {
-	if (lazy[idx] != 0) {
-		if (start != end) {
-			lazy[idx * 2] += lazy[idx];
-			lazy[idx * 2 + 1] += lazy[idx];
-		}
-		tree[idx] += (end - start + 1) * lazy[idx];
-		lazy[idx] = 0;
+template<typename T>
+class SegmentTree {
+public:
+	SegmentTree(int size) {
+		N = size;
+		tree.resize(size * 4); // roughly multiplied by 4
+		lazy.resize(size * 4);
 	}
 
-	if (l > end || r < start) return;
-
-	if (l <= start && end <= r) {
-		if (start != end) {
-			lazy[idx * 2] += val;
-			lazy[idx * 2 + 1] += val;
-		}
-		tree[idx] += (end - start + 1) * val;
-		return;
+	// update multiple elements
+	void update(int start, int end, T val) {
+		update(start, end, 1, N, 1, val);
 	}
 
-	int mid = (start + end) / 2;
-	update(idx * 2, start, mid , l, r, val);
-	update(idx * 2 + 1, mid + 1, end, l, r, val);
-	tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
-}
-
-ll getSUM(int idx, int start, int end, int l, int r) {
-	if (lazy[idx] != 0) {
-		if(start != end) {
-			lazy[idx * 2] += lazy[idx];
-			lazy[idx * 2 + 1] += lazy[idx];
-		}
-		tree[idx] += (end - start + 1) * lazy[idx];
-		lazy[idx] = 0;
+	// update single element
+	void update(int index, T val) {
+		update(index, index, val);
 	}
 
-	if (l > end || r < start) return 0;
-	if (l <= start && end <= r) return tree[idx];
-
-	int mid = (start + end) / 2;
-	return getSUM(idx * 2, start, mid, l, r) + getSUM(idx * 2 + 1, mid + 1, end, l, r);
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n, m, k; cin >> n >> m >> k;
-	d.resize(n + 1);
-	for (int i = 1; i <= n; i++) cin >> d[i];
-
-	int st;
-
-	for (st = 1; st < n; st *= 2);
-	st *= 2;
-
-	tree.resize(st);
-	lazy.resize(st);
-	init(1, 1, n);
-
-	for (int i = 0; i < m + k; i++) {
-		int q, l, r; cin >> q >> l >> r;
-		if (q == 1) {
-			ll v; cin >> v;
-			update(1, 1, n, l, r, v);
-		}
-		else cout << getSUM(1, 1, n, l, r) << "\n";
+	// get sum of range start to end
+	T query(int start, int end) {
+		return query(start, end, 1, N, 1);
 	}
-}
+
+private:
+	std::vector<T> tree; // segment tree, starting index is 1
+	std::vector<T> lazy; // lazy propagation
+	int N; // size of leaf node
+	
+	void lazyUpdate(int l, int r, int index) {
+		tree[index] += lazy[index] * (r - l + 1);
+		if (l != r) {
+			lazy[index * 2] += lazy[index];
+			lazy[index * 2 + 1] += lazy[index];
+		}
+		lazy[index] = 0;
+	}
+
+	T query(int start, int end, int l, int r, int index) {
+		lazyUpdate(l, r, index);
+		if (start > r || end < l) return 0;
+		if (start <= l && r <= end) return tree[index];
+		int mid = (l + r) / 2;
+		return query(start, end, l, mid, index * 2) + query(start, end, mid + 1, r, index * 2 + 1);
+	}
+
+	void update(int start, int end, int l, int r, int index, T val) {
+		lazyUpdate(l, r, index);
+		if (start > r || end < l) return;
+		else if (start <= l && r <= end) {
+			lazy[index] += val;
+			lazyUpdate(l, r, index);
+		}
+		else {
+			int mid = (l + r) / 2;
+			update(start, end, l, mid, index * 2, val);
+			update(start, end, mid + 1, r, index * 2 + 1, val);
+			tree[index] = tree[index * 2] + tree[index * 2 + 1];
+		}
+	}
+};
